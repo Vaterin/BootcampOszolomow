78aac59792321b8f06f51ef05b78009c76728b93
DESKTOP-2UVU6UO\Admin <vaterinn@gmail.com>
2023-08-19 21:30:19 +0200

    no message

---
+++ js/noten.js
@@ -0,0 +1,12 @@
+const asyncFunc = async () => {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+asyncFunc();
